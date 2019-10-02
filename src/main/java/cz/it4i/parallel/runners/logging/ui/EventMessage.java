9ba114175e8d65b86c6051d0c222a10d72acef53
Jan Kožusznik <jan@kozusznik.cz>
2019-10-02 15:54:51 +0200

    Add messages

---
+++ src/main/java/cz/it4i/parallel/runners/logging/ui/EventMessage.java
@@ -0,0 +1,29 @@
+
+package cz.it4i.parallel.runners.logging.ui;
+
+import cz.it4i.parallel.runners.RedirectingOutputService.OutputType;
+
+public class EventMessage {
+
+//Type of stream:
+	public enum StreamType {
+			OUTPUT, ERROR
+	}
+
+	private final OutputType msgcode;
+	private final String msg;
+
+	public EventMessage(OutputType msgcode, String msg) {
+
+		this.msgcode = msgcode;
+		this.msg = msg;
+	}
+
+	public OutputType getMsgcode() {
+		return msgcode;
+	}
+
+	public String getMsg() {
+		return msg;
+	}
+}
---
+++ src/main/java/cz/it4i/parallel/runners/logging/ui/FeedbackMessage.java
@@ -0,0 +1,9 @@
+package cz.it4i.parallel.runners.logging.ui;
+
+
+public class FeedbackMessage {
+	
+	public FeedbackMessage() {
+		// This is empty on purpose.
+	}
+}
